8879a8c4c8db2cd75190d70050bbf0c1c90cf6c7
Abigail <abigailrayan@gmail.com>
2026-06-03 16:28:16 +0530

    Increase guess attempts and enhance intro function
    Updated the game to allow 5 guesses instead of 3 and modified the intro function to differentiate between the first play and subsequent plays.

--- Random-Num-Guessing-Game/RNG-Game.py
+++ Random-Num-Guessing-Game/RNG-Game.py
@@ -2,19 +2,23 @@ import time
 import sys
 import random
 
-def intro():
-    print("\nWelcome to the Random No. Guessing Game!")
-    print("I will choose a number from 1 to 100. \nYou will have 3 chances to correctly guess the no. .\nIf you guess the number correctly within the given chances, you win.\nOtherwise, I win and you lose.")
-    time.sleep(5)
-    ans = input("Are you ready to play?(y/n)")
+def intro(firsttime):
+    if firsttime:
+        print("\nWelcome to the Random No. Guessing Game!")
+        print("I will choose a number from 1 to 100. \nYou will have 5 chances to correctly guess the no. .\nIf you guess the number correctly within the given chances, you win.\nOtherwise, I win and you lose.")
+        time.sleep(5)
+        ans = input("Are you ready to play?(y/n) ")
+    else:
+        ans = input("Do you want to play again?(y/n) ")
     return ans
-    
+
+# Game mechanics    
 def game():
     win_stat = False
     target = random.randint(1,100)
     time.sleep(2)
     print("\nThe number has been chosen.\nMay the guessing begin!")
-    for i in range(3):
+    for i in range(5):
         user_num = int(input("Enter no.:"))
         if user_num == target:
             print("Correct!")
@@ -26,15 +30,20 @@ def game():
             print("Too low!")
     return win_stat, target
 
-
-
 def exit_game():
     print("\nThanks for playing!")
     sys.exit(0)
 
-    
+# Application
 def app():
-    user_ans = intro()
+    # This ain't working, the value of "first" changed inside if ain't reflecting outside
+    first = True
+    if first:
+        user_ans = intro(True)
+        global first
+        first = False
+    else:
+        intro(False)
     if user_ans.lower() == 'y':
         win, target = game()
     else:
